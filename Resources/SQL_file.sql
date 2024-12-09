0e97360a54b2051cb5b8b7bf9935042588644d6e
SaraJZ <joulaeisara@gmail.com>
2024-12-09 18:52:15 +0330

    Add files via upload

---
+++ Resources/SQL_file.sql
@@ -0,0 +1,15 @@
+CREATE TABLE stroke_dataset (
+    id INT PRIMARY KEY,
+    gender VARCHAR(10),
+    age FLOAT,
+    hypertension BOOLEAN,
+    heart_disease BOOLEAN,
+    ever_married VARCHAR(5),
+    work_type VARCHAR(20),
+    residence_type VARCHAR(10),
+    avg_glucose_level FLOAT,
+    bmi FLOAT,
+    smoking_status VARCHAR(20),
+    stroke BOOLEAN
+);
+
